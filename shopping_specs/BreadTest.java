9e5b4bdf2d9aa91aa7ad18cbcc80296b8b1802f6
Glen Young <extractofcactus@icloud.com>
2017-07-22 22:31:02 +0100

    BreadTest setup

---
+++ shopping_specs/BreadTest.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+import shopping.*;
+
+public class BreadTest {
+  Bread bread;
+
+  @Before
+  public void before() {
+    bread = new Bread("Warbaton's", 2, "White")
+  }
+}
